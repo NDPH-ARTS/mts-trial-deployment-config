641db6d9c86af09f8c5e42c7d97327ca31f67200
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2020-12-29 22:06:10 +0200

    aligning the fhir server app name

--- terraform/trial_rg/modules/fhir/main.tf
+++ terraform/trial_rg/modules/fhir/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_app_service" "fhir_server" {
-  name                = "${var.trial_name}-fhir"
+  name                = "trial-${var.trial_name}-fhir"
   location            = var.location
   resource_group_name = var.rg_name
   app_service_plan_id = var.app_service_plan_id
