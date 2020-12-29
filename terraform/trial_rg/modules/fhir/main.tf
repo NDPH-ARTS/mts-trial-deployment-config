b36632c7719de91ca7944c5e6e8be92965109fd2
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2020-12-29 22:55:39 +0200

    adding fhir sql server and env vars

--- terraform/trial_rg/modules/fhir/main.tf
+++ terraform/trial_rg/modules/fhir/main.tf
@@ -9,6 +9,22 @@ resource "azurerm_app_service" "fhir_server" {
   }
 
   app_settings = {
-    always_on = "true"
+    always_on                                         = "true"
+    FHIRServer__Security__Enabled                     = "false"
+    SqlServer__ConnectionString                       = "Server=tcp:sql,1433;Initial Catalog=FHIR;Persist Security Info=False;User ID=${fhirsqluser};Password=${fhirsqlpassword};MultipleActiveResultSets=False;Connection Timeout=30;"
+    SqlServer__AllowDatabaseCreation                  = "true"
+    SqlServer__Initialize                             = "true"
+    SqlServer__SchemaOptions__AutomaticUpdatesEnabled = "true"
+    DataStore                                         = "SqlServer"
+    WEBSITES_PORT                                     = 8080
   }
+}
+
+resource "azurerm_sql_server" "fhir_sql_server" {
+  name                         = "${var.trial_name}fhirsqlserver"
+  location                     = var.location
+  resource_group_name          = var.rg_name
+  version                      = "12.0"
+  administrator_login          = "sa" # var.fhirsqluser
+  administrator_login_password = "temppassword123!" # var.fhirsqlpassword
 }
