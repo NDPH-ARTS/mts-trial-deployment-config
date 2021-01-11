91b3c0ef459ebdd83f530b3569ce6c307dce41fe
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2021-01-11 12:30:58 +0200

    making shared rg names unique

--- terraform/shared_rg/main.tf
+++ terraform/shared_rg/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_resource_group" "shared_rg" {
 # ---- Azure Monitor ----
 # Storage account
 resource "azurerm_storage_account" "shared_sa_appinsights" {
-  name                     = "saai${var.environment}"
+  name                     = "saaioxford${var.environment}"
   resource_group_name      = azurerm_resource_group.shared_rg.name
   location                 = var.location
   account_tier             = "Standard"
@@ -21,14 +21,14 @@ resource "azurerm_storage_account" "shared_sa_appinsights" {
 }
 
 resource "azurerm_application_insights" "shared_monitor_ai" {
-  name                = "ai-${var.environment}"
+  name                = "ai-oxford-${var.environment}"
   location            = var.location
   resource_group_name = azurerm_resource_group.shared_rg.name
   application_type    = "web"
 }
 
 resource "azurerm_log_analytics_workspace" "shared_monitor_analytics" {
-  name                = "la-${var.environment}"
+  name                = "la-oxford-${var.environment}"
   location            = var.location
   resource_group_name = azurerm_resource_group.shared_rg.name
   sku                 = "PerGB2018"
