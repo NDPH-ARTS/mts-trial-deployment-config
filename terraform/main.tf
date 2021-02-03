c3ea9c26cf26ad7051335c976244b102fe5ade1f
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2021-02-03 13:59:19 +0200

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,4 +22,7 @@ module "trial_rg" {
   sc_config_image_tag    = var.sc_config_image_tag
   sc_config_git_uri      = var.sc_config_git_uri
   sc_config_search_paths = var.sc_config_search_paths
+
+  sc_profile = var.sc_profile
+  sc_label   = var.sc_label
 }
