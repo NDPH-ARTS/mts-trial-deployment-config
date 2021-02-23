60d086d02bc4053ca04975be7625fc64fea176e3
Darren Smithson <33603539+darrengsmithson@users.noreply.github.com>
2021-02-23 18:28:13 +0000

    [arts-353] Adding managed identity to allow access inter-service communication - forcing build and deploy

--- terraform/trial_rg/services.tf
+++ terraform/trial_rg/services.tf
@@ -22,7 +22,7 @@ module "trial_app_service_site" {
   docker_image_tag    = var.site_image_tag
 
   settings = {
-    "initService.identity"                   = module.trial_app_service_init.identity
+    "INIT_SERVICE_IDENTITY"                   = module.trial_app_service_init.identity
     "SPRING_PROFILES_ACTIVE"                  = var.spring_profile
     "SPRING_CLOUD_CONFIG_LABEL"               = var.spring_config_label
     "SERVER_PORT"                             = "80"
@@ -54,7 +54,7 @@ module "trial_app_service_practitioner" {
 
   # todo use private endpoint
   settings = {
-    "initService.identity"                   = module.trial_app_service_init.identity
+    "INIT_SERVICE_IDENTITY"                   = module.trial_app_service_init.identity
     "SPRING_PROFILES_ACTIVE"                  = var.spring_profile
     "SPRING_CLOUD_CONFIG_LABEL"               = var.spring_config_label
     "SERVER_PORT"                             = "80"
@@ -89,7 +89,7 @@ module "trial_app_service_role" {
   docker_image_tag    = var.role_image_tag
 
   settings = {
-    "initService.identity"                   = module.trial_app_service_init.identity
+    "INIT_SERVICE_IDENTITY"                   = module.trial_app_service_init.identity
     "always_on"   = "true"
     "JDBC_DRIVER" = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
     # TODO: replace with KeyVault reference
