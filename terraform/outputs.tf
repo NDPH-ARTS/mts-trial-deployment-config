3d304a59f895f79304a13d0edc32a0d8400e3e5a
Alex Baxter <alex.baxter@ndph.ox.ac.uk>
2021-02-23 18:00:59 +0000

    Have to add a new output variable

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,9 @@ output "ui_conn_string" {
   description = "The UI storage account connection string."
   sensitive   = true
 }
+
+output "gateway_host" {
+  value       = module.trial_rg.modules.trial_sc_gateway.genericservice.hostname
+  description = "The hostname of the API gateway."
+  sensitive = false
+}
