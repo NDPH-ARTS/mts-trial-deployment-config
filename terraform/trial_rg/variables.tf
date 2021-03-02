181eb920d196b609fc242f66a335d6e792b3e8e9
Anat Balzam <anatbalzam@microsoft.com>
2021-03-02 12:20:58 +0200

    deploy new trial

--- terraform/trial_rg/variables.tf
+++ terraform/trial_rg/variables.tf
@@ -142,7 +142,7 @@ variable "init_client_id" {
   description = "The init client id."
   default     = "unknown"
   sensitive   = true
-
+}
 variable "github_ref" {
   type        = string
   description = "The ref that triggered this run. usually a branch name."
