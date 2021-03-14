3c7adcba7de0bedd4269eed47d564cdf282e11af
Guy Bertental <guybartal@gmail.com>
2021-03-14 15:30:15 +0200

    fix two outputs after merge

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,13 +11,13 @@ output "gateway_host" {
 }
 
 output "init_storage_conn_string" {
-  value       = module.trial_rg.init_storage_conn_string
+  value       = azurerm_storage_account.initstorageaccount.primary_connection_string
   description = "The init service storage account connection string."
   sensitive   = true
 }
 
 output "init_storage_share_name" {
-  value       = module.trial_rg.init_storage_share_name
+  value       = azurerm_storage_share.initstorageshare.name
   description = "The init service storage account share name."
   sensitive   = true
 }
