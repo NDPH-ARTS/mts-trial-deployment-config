e95ba6921d6b47764e3f016246278f6d537dd42f
Guy Bertental <guybartal@gmail.com>
2021-03-16 22:05:32 +0200

    guy29.10 count

--- terraform/modules/genericservice/main.tf
+++ terraform/modules/genericservice/main.tf
@@ -31,7 +31,8 @@ resource "azurerm_app_service" "generic_service" {
   app_settings = var.settings
 
   dynamic "storage_account" {
-    for_each = var.storage_account != null ? [var.storage_account] : []
+    count    = var.storage_account == null ? 0 : 1
+    for_each = var.storage_account
     content {
       name         = storage_account.value["name"]
       type         = storage_account.value["type"]
